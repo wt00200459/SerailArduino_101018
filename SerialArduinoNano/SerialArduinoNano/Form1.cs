1260bace88246aab397a34d462131ff9ea918040
wt00200459 <boss.2587@hotmail.co.th>
2018-10-10 10:38:46 +0700

    addComboBox to select Port and Speed

--- SerialArduinoNano/SerialArduinoNano/Form1.cs
+++ SerialArduinoNano/SerialArduinoNano/Form1.cs
@@ -6,13 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO.Ports;
+
 using System.Windows.Forms;
 
 namespace SerialArduinoNano
-{
+{using System.IO.Ports;
     public partial class Form1 : Form
     {
+       
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +24,63 @@ namespace SerialArduinoNano
             string[] ports = SerialPort.GetPortNames();
             listBox1.Items.Clear();
             listBox1.Items.AddRange(ports);
-
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(ports);
             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+   
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = Int32.Parse(comboBox2.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                return;
+            }else
+            {
+                serialPort1.Open();
+                button2.Enabled = false;
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                return;
+
+            }else
+            {
+                serialPort1.Close();
+                button2.Enabled = true;
+                button3.Enabled = false;
+            }
         }
     }
 }
